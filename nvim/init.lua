cc1b791318a3bc4f71d165a3fd0cc9c4f827503d
Guillaume Robin <guillaume@inarix.com>
2024-10-11 22:21:35 +0200

    Add nvim and zsh config files

---
+++ nvim/init.lua
@@ -0,0 +1,158 @@
+-- Note: <D- is mapped to cmd key
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- Fuzzy finder
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim', { tag='0.1.8' })
+
+-- Theme
+Plug('catppuccin/nvim', { as='catppuccin' })
+
+-- LSP
+Plug('neovim/nvim-lspconfig')
+
+-- Debugging
+Plug('puremourning/vimspector')
+
+-- Completion
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('hrsh7th/nvim-cmp')
+
+-- Commenter
+Plug('terrortylor/nvim-comment')
+
+-- Formatter
+Plug('stevearc/conform.nvim')
+
+-- Todos
+Plug('folke/todo-comments.nvim')
+
+vim.call('plug#end')
+
+-- Mapping function
+local function map(mode, lhs, rhs, opts)
+local options = {noremap = true}
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+-- Find files using Telescope command-line sugar.
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+
+-- set color scheme
+-- colorscheme catppuccin  catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+vim.cmd.colorscheme('catppuccin')
+
+-- Escape to exit terminal
+map('t', '<Esc>', '<C-\\><C-n>')
+
+-- hybrid line number
+vim.cmd.set('number', 'relativenumber')
+vim.cmd.set('nu', 'rnu')
+
+-- Set up nvim-cmp.
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+-- Set configuration for specific filetype.
+--[[ cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'git' },
+  }, {
+    { name = 'buffer' },
+  })
+)
+equire("cmp_git").setup() ]]-- 
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- deno
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+vim.g.markdown_fenced_languages = {
+  "ts=typescript"
+}
+
+local nvim_lsp = require('lspconfig')
+nvim_lsp.denols.setup {
+  on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+  capabilities = capabilities
+}
+
+-- Setup Commenter
+require('nvim_comment').setup()
+map('n', '<D-/>', '<cmd>CommentToggle<cr>')
+
+-- Format
+require("conform").setup({
+  formatters_by_ft = {
+    typescript = { "deno fmt" },
+    javascript = { "deno fmt" }
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+})
+
+-- Todos
+require("todo-comments").setup {}
