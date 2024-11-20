45eb1e5e9e105cc42631daee525cca5f1298fc80
Guillaume Robin <gdream.dev@gmail.com>
2024-11-20 20:55:47 +0100

    Add js-debug-adapter to mason.lua

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,29 +1,3 @@
--- (utils): merge arrays
-function table.contains(table, element)
-  for _, value in pairs(table) do
-    if value == element then
-      return true
-    end
-  end
-  return false
-end
-function table.merge_list(t1, t2)
-  for k,v in ipairs(t2) do
-    table.insert(t1, v)
-  end 
-  
-  return t1
-end
-function table.merge_list_unique(t1, t2)
-  for k,v in ipairs(t2) do
-    if not table.contains(t1, v) then
-      table.insert(t1, v)
-    end
-  end 
-
-  return t1
-end
-
 return {
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
@@ -35,6 +9,7 @@ return {
       opts.ensure_installed = {
         "codelldb",
         "cpptools",
+        "js-debug-adapter",
       }
     end,
   }
