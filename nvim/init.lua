f1e33104335ed4276b0ec3330ce47953c8718168
Guillaume Robin <gdream.dev@gmail.com>
2024-11-25 21:45:56 +0100

    Use mason for c# lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,7 +108,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- deno
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
 vim.g.markdown_fenced_languages = {
   "ts=typescript",
   "rs=rust"
@@ -134,57 +134,60 @@ map('n', '<D-/>', '<cmd>CommentToggle<cr>')
 map('n', '<Shift-f>', '<cmd>Neoformat<cr>')
 
 -- C#
-vim.g.OmniSharp_server_use_net6 = 1
-
-nvim_lsp.omnisharp.setup({
-  capabilities = capabilities,
+-- For future me: when opening a Godot project, make sure to first build it one time before doubting about the lsp.
+nvim_lsp.omnisharp.setup {
+  cmd = { "dotnet", HOME .. "/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
   handlers = {
     ["textDocument/definition"] = require("omnisharp_extended").handler,
   },
-  cmd = { "dotnet", HOME .. "/.bin/omnisharp-osx-x64-net6.0/OmniSharp.dll" },
-  
-  -- Enables support for reading code style, naming convention and analyzer
-  -- settings from .editorconfig.
-  enable_editorconfig_support = true,
-  
-  -- If true, MSBuild project system will only load projects for files that
-  -- were opened in the editor. This setting is useful for big C# codebases
-  -- and allows for faster initialization of code navigation features only
-  -- for projects that are relevant to code that is being edited. With this
-  -- setting enabled OmniSharp may load fewer projects and may thus display
-  -- incomplete reference lists for symbols.
-  enable_ms_build_load_projects_on_demand = false, -- default false
-  
-  -- Enables support for roslyn analyzers, code fixes and rulesets.
-  enable_roslyn_analyzers = true, -- default false
-  
-  -- Specifies whether 'using' directives should be grouped and sorted during
-  -- document formatting.
-  organize_imports_on_format = true, -- default false
-  
-  -- Enables support for showing unimported types and unimported extension
-  -- methods in completion lists. When committed, the appropriate using
-  -- directive will be added at the top of the current file. This option can
-  -- have a negative impact on initial completion responsiveness,
-  -- particularly for the first few completion sessions after opening a
-  -- solution.
-  enable_import_completion = true, -- default false
-  
-  -- Specifies whether to include preview versions of the .NET SDK when
-  -- determining which version to use for project loading.
-  sdk_include_prereleases = true,
-  
-  -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-  -- true
-  analyze_open_documents_only = true, -- default false
-})
+  root_dir = nvim_lsp.util.root_pattern("*.csproj"),
+  capabilities = capabilities,
+  settings = {
+    FormattingOptions = {
+      -- Enables support for reading code style, naming convention and analyzer
+      -- settings from .editorconfig.
+      EnableEditorConfigSupport = true,
+      -- Specifies whether 'using' directives should be grouped and sorted during
+      -- document formatting.
+      OrganizeImports = true,
+    },
+    MsBuild = {
+      -- If true, MSBuild project system will only load projects for files that
+      -- were opened in the editor. This setting is useful for big C# codebases
+      -- and allows for faster initialization of code navigation features only
+      -- for projects that are relevant to code that is being edited. With this
+      -- setting enabled OmniSharp may load fewer projects and may thus display
+      -- incomplete reference lists for symbols.
+      LoadProjectsOnDemand = false,
+    },
+    RoslynExtensionsOptions = {
+      -- Enables support for roslyn analyzers, code fixes and rulesets.
+      EnableAnalyzersSupport = true,
+      -- Enables support for showing unimported types and unimported extension
+      -- methods in completion lists. When committed, the appropriate using
+      -- directive will be added at the top of the current file. This option can
+      -- have a negative impact on initial completion responsiveness,
+      -- particularly for the first few completion sessions after opening a
+      -- solution.
+      EnableImportCompletion = true,
+      -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+      -- true
+      AnalyzeOpenDocumentsOnly = true,
+    },
+    Sdk = {
+      -- Specifies whether to include preview versions of the .NET SDK when
+      -- determining which version to use for project loading.
+      IncludePrereleases = true,
+    },
+  },
+}
 
 -- Treesitter
 local configs = require("nvim-treesitter.configs")
 
 configs.setup {
   -- Add a language of your choice
-  ensure_installed = {"lua", "javascript", "typescript", "rust", "go", "terraform", "json"},
+  ensure_installed = {"lua", "javascript", "typescript", "rust", "go", "terraform", "json", "c_sharp"},
   sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
@@ -307,6 +310,23 @@ dap.configurations.typescript = {
     attachSimplePort = 9229,
   },
 }
+
+-- nvim-dap godot
+-- Use mason to install netcoredbg
+dap.adapters.godot = {
+  type = 'server',
+  request = 'launch',
+  port = 6007
+}
+dap.configurations.cs = {
+  {
+    type = "godot",
+    request = "launch",
+    name = "Launch scene",
+    project = "${workspaceFolder}",
+  }
+}
+
 
 -- nvim-dap-projects
 -- Setup nvim-dap per project using root/.nvim-dap.lua
