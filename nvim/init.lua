130907a2ba93b810df012fb626036609bafc8470
Guillaume Robin <guillaume@inarix.com>
2025-11-10 12:08:20 +0100

    Fix conflict between ts_ls and denols

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,8 +2,6 @@
 require("config.lazy")
 -- To add new lsp: add them in lua/config/mason.lua ensure_installed.
 -- All lsp are automatically enabled by mson-lspconfig.
--- If you need to override the lsp configuration, use the lsp folder with
--- the lsp ID as file name. E.g: lsp/lua_ls.lua
 require("config.mason")
 require("config.header")
 require("config.indent-blankline")
@@ -11,3 +9,9 @@ require("config.indent-blankline")
 require("core.config")
 require("core.mappings")
 require("core.hooks")
+
+-- If you need to override the lsp configuration, use the lua/lsp folder with
+-- the lsp ID as file name. E.g: lua/lsp/lua_ls.lua
+-- and require it. Take as example lua/lsp/ts_ls.lua
+require("lsp.ts_ls")
+require("lsp.denols")
